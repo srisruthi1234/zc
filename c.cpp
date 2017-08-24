b61e753a70c43029848156f0cc71faeae5745102
srisruthi1234 <srisruthi147@gmail.com>
2017-08-24 09:07:25 +0530

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+int g;
+cout<<" enter number "<<endl;
+cin>>n;
+cout<<" enter number "<<endl;
+cin>>g;
+cout<<"*********************************"<<endl;
+cout<<" programology student site "<<endl;
+cout<<"**********************************"<<endl;
+for(;n<=g;)
+{
+cout<<n<<endl;
+n=n+2;
+}
+}
